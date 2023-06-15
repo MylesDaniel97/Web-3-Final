2ede1868829f3a98414e31282b6cbbde7efaad52
mdaniel-devonte <57999069+mdaniel-devonte@users.noreply.github.com>
2023-06-15 14:47:35 -0400

    Final edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -18,67 +18,77 @@ mainMenu.addEventListener("click", () => {
         mainMenu.classList.remove("show-menu");
         canSeeMenu = false;
 })
-var QtyInput = (function () {
-	var $qtyInputs = $(".qty-input");
 
-	if (!$qtyInputs.length) {
-		return;
-	}
 
-	var $inputs = $qtyInputs.find(".product-qty");
-	var $countBtn = $qtyInputs.find(".qty-count");
-	var qtyMin = parseInt($inputs.attr("min"));
-	var qtyMax = parseInt($inputs.attr("max"));
 
-	$inputs.change(function () {
-		var $this = $(this);
-		var $minusBtn = $this.siblings(".qty-count--minus");
-		var $addBtn = $this.siblings(".qty-count--add");
-		var qty = parseInt($this.val());
+// Store references that all functions can use.
+var resultEl = document.querySelector(".resultSet"),
+  plusMinusWidgets = document.querySelectorAll(".qty-input");
 
-		if (isNaN(qty) || qty <= qtyMin) {
-			$this.val(qtyMin);
-			$minusBtn.attr("disabled", true);
-		} else {
-			$minusBtn.attr("disabled", false);
-			
-			if(qty >= qtyMax){
-				$this.val(qtyMax);
-				$addBtn.attr('disabled', true);
-			} else {
-				$this.val(qty);
-				$addBtn.attr('disabled', false);
-			}
-		}
-	});
+// Attach the handlers to each plus-minus thing
+for (var i = 0; i < plusMinusWidgets.length; i++) {
+  plusMinusWidgets[i].querySelector(".qty-count--minus").addEventListener("click", clickHandler);
+  plusMinusWidgets[i].querySelector(".qty-count--add").addEventListener("click", clickHandler);
+  plusMinusWidgets[i].querySelector(".product-qty").addEventListener("change", changeHandler);
+}
 
-	$countBtn.click(function () {
-		var operator = this.dataset.action;
-		var $this = $(this);
-		var $input = $this.siblings(".product-qty");
-		var qty = parseInt($input.val());
+/*****
+ * both plus and minus use the same function, but value is set by the class of the
+ *  button
+ *****/
+function clickHandler(event) {
+  // reference to the count input field
+  var countEl = event.target.parentNode.querySelector(".product-qty");
+  if (event.target.className.match(/\bqty-count--minus\b/)) {
+    countEl.value = Number(countEl.value) - 1;
+  } else if (event.target.className.match(/\bqty-count--add\b/)) {
+    countEl.value = Number(countEl.value) + 1;
+    
+  }
+  // When we programmatically change the value, we need to manually trigger
+  //  the change event.
+  triggerEvent(countEl, "change");
+};
 
-		if (operator == "add") {
-			qty += 1;
-			if (qty >= qtyMin + 1) {
-				$this.siblings(".qty-count--minus").attr("disabled", false);
-			}
+/*****
+ * changeHandler() processes whenever a plusMinusWidget's count el is changed.
+ *  It iterates over all plusMinusWidgets, gets their count, and outputs that
+ *  to the given resultEl input field.
+ *****/
+function changeHandler(event) {
+  // remove all value from the result el.
+  resultEl.value = 0;
+  /******
+   * Here is the only functional change, per your comment. Rather
+   *  concatenating a string, you want to sum values of the 
+   *  plusMinusWidget. To do this, we need to cast the value of each
+   *  plusMinusWidget to a Number value, and add that to the Number
+   *  value of the resultEl.
+   *****/
+  for (var i = 0; i < plusMinusWidgets.length; i++) {
+    // Add the current plusMinusWidget value to the resultEl value.
+    resultEl.value = Number(resultEl.value) + Number(plusMinusWidgets[i].querySelector('.product-qty').value);
 
-			if (qty >= qtyMax) {
-				$this.attr("disabled", true);
-			}
-		} else {
-			qty = qty <= qtyMin ? qtyMin : (qty -= 1);
-			
-			if (qty == qtyMin) {
-				$this.attr("disabled", true);
-			}
+  }
 
-			if (qty < qtyMax) {
-				$this.siblings(".qty-count--add").attr("disabled", false);
-			}
-		}
+};
 
-		$input.val(qty);
-	});
-})();
+/*****
+ * triggerEvent() -- function to trigger an HTMLEvent on a given element.
+ *  similar to jquery's trigger(), simply a convenience function. Not the
+ *  point of this exercise.
+ *****/
+
+function triggerEvent(el, type){
+   if ('createEvent' in document) {
+        // modern browsers, IE9+
+        var e = document.createEvent('HTMLEvents');
+        e.initEvent(type, false, true);
+        el.dispatchEvent(e);
+    } else {
+        // IE 8
+        var e = document.createEventObject();
+        e.eventType = type;
+        el.fireEvent('on'+e.eventType, e);
+    }
+}
